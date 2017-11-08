afbb88d3697185c9caf6ca944820426e06265ae9
bipinkh <bipinkh91@gmail.com>
2017-11-08 13:45:56 +0545

    generated dao files using daogenerator

---
+++ app/src/main/java/com/bipinkh/dbapp/helper/daoGenerator/DaoFilesGenerator.java
@@ -0,0 +1,45 @@
+package com.bipinkh.dbapp.helper.daoGenerator;
+
+/**
+ * Created by bipin on 11/8/2017.
+ */
+
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Schema;
+
+public class DaoFilesGenerator {
+
+
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "com.bipinkh.dbapp.models.database"); // package name + db folder
+        schema.enableKeepSectionsByDefault();
+        //make tables
+        addTables(schema);
+        //generate
+        try {
+            new DaoGenerator().generateAll(schema,"./app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTables(final Schema schema) {
+        addUserEntities(schema);
+        //add other tables here
+    }
+
+    // This is use to describe the colums of your table
+    private static Entity addUserEntities(final Schema schema) {
+        Entity user = schema.addEntity("User"); //table
+        user.addIdProperty().primaryKey().autoincrement();
+        user.addStringProperty("first_name");
+        user.addStringProperty("last_name");
+        user.addLongProperty("phone");
+        user.addStringProperty("address");
+        user.addStringProperty("email");
+        user.addStringProperty("gender");
+        return user;
+    }
+
+}
