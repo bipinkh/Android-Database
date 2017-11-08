0ec50d443125961ddf98be371f0d3eec8486e38d
bipinkh <bipinkh91@gmail.com>
2017-11-08 13:57:54 +0545

    application class files updated for getting daosession

--- app/src/main/java/com/bipinkh/dbapp/dbapp.java
+++ app/src/main/java/com/bipinkh/dbapp/dbapp.java
@@ -2,6 +2,11 @@ package com.bipinkh.dbapp;
 
 import android.app.Application;
 
+import com.bipinkh.dbapp.models.database.DaoMaster;
+import com.bipinkh.dbapp.models.database.DaoSession;
+
+import org.greenrobot.greendao.database.Database;
+
 /**
  * Created by bipin on 11/8/2017.
  */
@@ -10,12 +15,25 @@ import android.app.Application;
 public class dbapp extends Application {
 
     private static dbapp appinstancce;
+    DaoSession daoSession;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        initializeDaoSession();
     }
 
-    public static dbapp getAppInstance()  {   return appinstancce; }
+    public void initializeDaoSession(){
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this,"users-db");
+        Database db = helper.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+    }
+
+    public static dbapp getAppInstance()  {   return appinstancce;  }
+
+    public DaoSession getDaoSession(){
+        if (daoSession==null)   {   initializeDaoSession();   }
+        return daoSession;
+    }
 
 }
